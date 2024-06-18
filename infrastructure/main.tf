0fffc8c0bd52c3bb70d980ea10aece426314ab71
tee118 <tejufamuyiwa@gmail.com>
2024-06-19 00:21:37 +0100

    added a new feature some files changed

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -22,70 +22,63 @@ resource "aws_route_table" "main" {
 }
 
 resource "aws_subnet" "subnet" {
-  count             = 2
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
+  count = 2
+  vpc_id = aws_vpc.main.id
+  cidr_block = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
   availability_zone = data.aws_availability_zones.available.names[count.index]
   map_public_ip_on_launch = true
 }
 
 resource "aws_route_table_association" "main" {
   count = 2
-  subnet_id      = aws_subnet.subnet[count.index].id
+  subnet_id = aws_subnet.subnet[count.index].id
   route_table_id = aws_route_table.main.id
 }
 
 resource "aws_security_group" "main" {
   vpc_id = aws_vpc.main.id
-  name   = "ecs-security-group"
+  name = "ecs-security-group"
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
 resource "aws_rds_cluster" "default" {
-  cluster_identifier      = "my-cluster"
-  engine                  = "aurora-mysql"
-  master_username         = var.db_username
-  master_password         = var.db_password
-  database_name           = var.db_name
+  cluster_identifier = "my-cluster"
+  engine = "aurora-mysql"
+  master_username = var.db_username
+  master_password = var.db_password
+  database_name = var.db_name
   backup_retention_period = 5
   preferred_backup_window = "07:00-09:00"
-  skip_final_snapshot     = true
+  skip_final_snapshot = true
 
   vpc_security_group_ids = [aws_security_group.main.id]
-
   db_subnet_group_name = aws_db_subnet_group.main.name
 }
 
 resource "aws_db_subnet_group" "main" {
-  name       = "my-db-subnet-group"
+  name = "my-db-subnet-group"
   subnet_ids = aws_subnet.subnet[*].id
 }
 
 resource "aws_ecr_repository" "backend" {
   name = "my-backend-repo"
-  # lifecycle {
-  #   prevent_destroy = true
-  # }
 }
 
 resource "aws_ecr_repository" "frontend" {
   name = "my-frontend-repo"
-  # lifecycle {
-  #   prevent_destroy = true
-  # }
 }
 
 resource "aws_iam_role" "ecs_task_execution_role" {
@@ -102,14 +95,34 @@ resource "aws_iam_role" "ecs_task_execution_role" {
       }
     ]
   })
-  # lifecycle {
-  #   prevent_destroy = true
-  # }
 }
 
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
-  role       = aws_iam_role.ecs_task_execution_role.name
+  role = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_iam_role_policy" "ecr_pull_policy" {
+  name = "ecr_pull_policy"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage",
+          "ecr:GetAuthorizationToken"
+        ]
+        Effect = "Allow"
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecr_pull_policy_attachment" {
+  role = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = aws_iam_role_policy.ecr_pull_policy.arn
 }
 
 resource "aws_ecs_cluster" "my_cluster" {
@@ -117,45 +130,45 @@ resource "aws_ecs_cluster" "my_cluster" {
 }
 
 resource "aws_ecs_task_definition" "backend_task_def" {
-  family                   = "my-backend-task"
-  network_mode             = "awsvpc"
+  family = "my-backend-task"
+  network_mode = "awsvpc"
   requires_compatibilities = ["FARGATE"]
-  cpu                      = "256"
-  memory                   = "512"
-  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  cpu = "256"
+  memory = "512"
+  execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
 
   container_definitions = jsonencode([
     {
-      name      = "my-backend-container"
-      image     = "${data.aws_caller_identity.current.account_id}.dkr.ecr.eu-west-2.amazonaws.com/my-backend:latest"
-      cpu       = 256
-      memory    = 512
+      name = "my-backend-container"
+      image = "${data.aws_caller_identity.current.account_id}.dkr.ecr.eu-west-2.amazonaws.com/my-backend:latest"
+      cpu = 256
+      memory = 512
       essential = true
       portMappings = [
         {
           containerPort = 8080
-          hostPort      = 8080
+          hostPort = 8080
         }
       ]
       environment = [
         {
-          name  = "DB_HOST"
+          name = "DB_HOST"
           value = aws_rds_cluster.default.endpoint
         },
         {
-          name  = "DB_PORT"
+          name = "DB_PORT"
           value = "3306"
         },
         {
-          name  = "DB_NAME"
+          name = "DB_NAME"
           value = var.db_name
         },
         {
-          name  = "DB_USERNAME"
+          name = "DB_USERNAME"
           value = var.db_username
         },
         {
-          name  = "DB_PASSWORD"
+          name = "DB_PASSWORD"
           value = var.db_password
         }
       ]
@@ -164,24 +177,24 @@ resource "aws_ecs_task_definition" "backend_task_def" {
 }
 
 resource "aws_ecs_task_definition" "frontend_task_def" {
-  family                   = "my-frontend-task"
-  network_mode             = "awsvpc"
+  family = "my-frontend-task"
+  network_mode = "awsvpc"
   requires_compatibilities = ["FARGATE"]
-  cpu                      = "256"
-  memory                   = "512"
-  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  cpu = "256"
+  memory = "512"
+  execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
 
   container_definitions = jsonencode([
     {
-      name      = "my-frontend-container"
-      image     = "${data.aws_caller_identity.current.account_id}.dkr.ecr.eu-west-2.amazonaws.com/my-frontend:latest"
-      cpu       = 256
-      memory    = 512
+      name = "my-frontend-container"
+      image = "${data.aws_caller_identity.current.account_id}.dkr.ecr.eu-west-2.amazonaws.com/my-frontend:latest"
+      cpu = 256
+      memory = 512
       essential = true
       portMappings = [
         {
           containerPort = 80
-          hostPort      = 80
+          hostPort = 80
         }
       ]
     }
@@ -189,13 +202,13 @@ resource "aws_ecs_task_definition" "frontend_task_def" {
 }
 
 resource "aws_ecs_service" "backend_service" {
-  name            = "my-backend-service"
-  cluster         = aws_ecs_cluster.my_cluster.id
+  name = "my-backend-service"
+  cluster = aws_ecs_cluster.my_cluster.id
   task_definition = aws_ecs_task_definition.backend_task_def.arn
-  desired_count   = 1
+  desired_count = 1
 
   network_configuration {
-    subnets         = aws_subnet.subnet[*].id
+    subnets = aws_subnet.subnet[*].id
     security_groups = [aws_security_group.main.id]
   }
 
@@ -203,13 +216,13 @@ resource "aws_ecs_service" "backend_service" {
 }
 
 resource "aws_ecs_service" "frontend_service" {
-  name            = "my-frontend-service"
-  cluster         = aws_ecs_cluster.my_cluster.id
+  name = "my-frontend-service"
+  cluster = aws_ecs_cluster.my_cluster.id
   task_definition = aws_ecs_task_definition.frontend_task_def.arn
-  desired_count   = 1
+  desired_count = 1
 
   network_configuration {
-    subnets         = aws_subnet.subnet[*].id
+    subnets = aws_subnet.subnet[*].id
     security_groups = [aws_security_group.main.id]
   }
 
