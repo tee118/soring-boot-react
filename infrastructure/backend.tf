d5eaa2d2b7dfc7ee9994c0b6b5e406aacf3aaef4
tee118 <tejufamuyiwa@gmail.com>
2024-06-18 11:26:32 +0100

    test commit

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -1,8 +1,18 @@
-terraform {
-  backend "s3" {
-    bucket         = "statelockterraform"
-    key            = "terraform.tfstate"
-    region         = "eu-west-2"
-    dynamodb_table = "terraform-lock-table"
+provider "aws" {
+  region = "eu-west-2"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "statelockterraform"
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-lock-table"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
   }
 }
--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -5,14 +5,17 @@ provider "aws" {
 # Fetch current account ID
 data "aws_caller_identity" "current" {}
 
+# Fetch available availability zones
+data "aws_availability_zones" "available" {}
+
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
 
 resource "aws_subnet" "subnet" {
-  count = 2
-  vpc_id     = aws_vpc.main.id
-  cidr_block = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
+  count             = 2
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
   availability_zone = data.aws_availability_zones.available.names[count.index]
 }
 
@@ -79,21 +82,6 @@ resource "aws_iam_role" "ecs_task_execution_role" {
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
-
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "statelockterraform"
-}
-
-resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "terraform-lock-table"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
 }
 
 resource "aws_ecs_cluster" "my_cluster" {
