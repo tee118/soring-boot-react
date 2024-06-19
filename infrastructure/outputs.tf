d885c8a7648052896845aca8254a2c0e491c7f73
tee118 <tejufamuyiwa@gmail.com>
2024-06-19 01:37:37 +0100

    added a new feature some files changed

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -2,8 +2,12 @@ output "vpc_id" {
   value = aws_vpc.main.id
 }
 
-output "subnet_ids" {
-  value = aws_subnet.subnet[*].id
+output "public_subnet_ids" {
+  value = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+  value = aws_subnet.private[*].id
 }
 
 output "security_group_id" {
