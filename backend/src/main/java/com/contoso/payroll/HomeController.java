1f7582ebb38c430700bcf252923a4ec1f734c4b9
tee118 <tejufamuyiwa@gmail.com>
2024-06-30 23:19:02 +0100

    added a new feature some files changed

--- backend/src/main/java/com/contoso/payroll/HomeController.java
+++ backend/src/main/java/com/contoso/payroll/HomeController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
-	@RequestMapping(value = "/")
-	public String index() {
-		return "index";
-	}
+    @RequestMapping(value = "/{path:[^\\.]*}")
+    public String forward() {
+        return "forward:/";
+    }
 
 }
