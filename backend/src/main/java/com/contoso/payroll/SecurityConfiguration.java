872f0f77f5e9fb475cf8efbe3510b88fac84686d
tee118 <tejufamuyiwa@gmail.com>
2024-07-08 19:09:11 +0100

    added a new feature some files changed

--- backend/src/main/java/com/contoso/payroll/SecurityConfiguration.java
+++ backend/src/main/java/com/contoso/payroll/SecurityConfiguration.java
@@ -1,47 +1,60 @@
-// package com.contoso.payroll;
+package com.contoso.payroll;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-// import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-// import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-// import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-// @Configuration
-// @EnableWebSecurity
-// @EnableGlobalMethodSecurity(prePostEnabled = true)
-// public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-//     @Autowired
-//     private SpringDataJpaUserDetailsService userDetailsService;
+    @Autowired
+    private SpringDataJpaUserDetailsService userDetailsService;
 
-//     @Override
-//     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//         auth
-//             .userDetailsService(this.userDetailsService)
-//             .passwordEncoder(Manager.PASSWORD_ENCODER());
-//     }
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+            .userDetailsService(this.userDetailsService)
+            .passwordEncoder(passwordEncoder())
+            .and()
+            .inMemoryAuthentication()
+            .withUser("user")
+            .password(passwordEncoder().encode("password"))
+            .roles("USER");
+    }
 
-//     @Override
-//     protected void configure(HttpSecurity http) throws Exception {
-//         http
-//             .authorizeRequests()
-//                 .antMatchers("/built/**", "/main.css").permitAll()
-//                 .anyRequest().authenticated()
-//                 .and()
-//             .formLogin()
-//                 .defaultSuccessUrl("/", true)
-//                 .permitAll()
-//                 .and()
-//             .httpBasic()
-//                 .and()
-//             .csrf().disable()
-//             .logout()
-//                 .logoutSuccessUrl("/");
-//     }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+                .antMatchers("/built/**", "/main.css").permitAll()
+                .anyRequest().authenticated()
+                .and()
+            .formLogin()
+                .defaultSuccessUrl("/", true)
+                .permitAll()
+                .and()
+            .httpBasic()
+                .and()
+            .csrf().disable()
+            .logout()
+                .logoutSuccessUrl("/");
+    }
 
-//     public PasswordEncoder passwordEncoder() {
-//         return new BCryptPasswordEncoder();
-//     }
-// }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
